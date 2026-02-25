65cd7326977a7c6291d998732e0f7be4a29a0c2e
livenintendoswitch <leonardsuwanta3@gmail.com>
2026-02-25 16:47:10 +0700

    fixed env bug + new script setup

---
+++ laravel/setup.sh
@@ -0,0 +1,16 @@
+composer install
+
+cp -n .env.example .env
+
+php artisan key:generate
+
+sed -i 's/^#* *DB_CONNECTION=.*/DB_CONNECTION=mysql/' .env
+sed -i 's/^#* *DB_HOST=.*/DB_HOST=mysql/' .env
+sed -i 's/^#* *DB_PORT=.*/DB_PORT=3306/' .env
+sed -i 's/^#* *DB_DATABASE=.*/DB_DATABASE=POSdatabase/' .env
+sed -i 's/^#* *DB_USERNAME=.*/DB_USERNAME=admin/' .env
+sed -i 's/^#* *DB_PASSWORD=.*/DB_PASSWORD=123456789/' .env
+
+php artisan artisan migrate
+
+php artisan serve --host=0.0.0.0 --port=8000
